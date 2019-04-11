43104b17e010d32d81cb0c8cdcf4c269fcf03ac3
Dragomir Mildov <drashoo@abv.bg>
2019-04-11 17:11:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express()
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send({ test01: 'test00001'});
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('server listen on port: ' + PORT);
+});
